71ff47ade0f162c9dc868733119359877254f354
timdnoe <timdnoe@users.noreply.github.com>
2024-03-18 19:47:40 -0400

    Update script2.js

--- script2.js
+++ script2.js
@@ -294,7 +294,7 @@ function applyColorTheme() {
 
     // Insert proper icon
     const searchIcon = document.getElementById('search-icon');
-    searchIcon.fill = theme.secondaryColor;
+    searchIcon.style.fill = theme.secondaryColor;
 
     //Search Input
     const searchInput = document.getElementById('search-input'); // Reference to the input field
