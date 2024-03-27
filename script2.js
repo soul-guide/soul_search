3cc8d3e136754dc0e3f5a268ddb0238815d964e6
timdnoe <timdnoe@users.noreply.github.com>
2024-03-27 11:36:05 -0400

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,10 +1,3 @@
-// Generate question buttons on page load
-window.onload = () => {
-    loadHtml('https://soulguide.github.io/soul-search/index3.html')
-      .then(loadingItems)
-      .catch(error => console.error('Error in loadHtml or performSearch:', error));
-};
-
 function loadHtml(url) {
     return new Promise((resolve, reject) => {
         fetch(url)
@@ -57,9 +50,7 @@ function decodeForHTMLAttribute(str, json=false) {
         .replace(/&quot;/g, '"') // Encode double quotes
         .replace(/&#39;/g, "'")   // Encode single quotes (apostrophes)
         .replace(/&lt;/g, '<')    // Encode less than
-        .replace(/&gt;/g, '>')   // Encode greater than
-        .replace(/%7B;/g, '{')
-        .replace(/%7D;/g, '}')
+        .replace(/&gt;/g, '>');   // Encode greater than
     if(!json){
         return str.split('%^%')
     }
@@ -93,16 +84,19 @@ function generateQuestionButtons() {
     
 }
 
+
+// Generate question buttons on page load
+window.onload = () => {
+    loadHtml('https://soulguide.github.io/soul-search/index2.html')
+      .then(loadingItems)
+      .catch(error => console.error('Error in loadHtml or performSearch:', error));
+};
+
 function updateText(){
     let title = document.getElementById('soulsearch').getAttribute('title')
-    let logo = document.getElementById('soulsearch').getAttribute('logo')
     let subtitle = document.getElementById('soulsearch').getAttribute('subtitle')
-    document.getElementById('soulsearch-title').innerHTML = `<p>${title}</p>`;
-    console.log("logo",logo)
-    if (logo){
-        document.getElementById('soulsearch-title').innerHTML = `<img src="${logo}">`;
-    }
-    document.getElementById('soulsearch-subtitle').innerHTML = `<p>${subtitle}</p>`;
+    document.getElementById('soulsearch-title').innerHTML = title;
+    document.getElementById('soulsearch-subtitle').innerHTML = subtitle;
 }
 
 function loadingItems(){
@@ -110,19 +104,6 @@ function loadingItems(){
     applyColorTheme();
     updateText();
     performSearch();
-}
-
-function trigger_freeze(frozen=true){
-    if(frozen){
-        document.getElementById("soulsearch-inner").style.pointerEvents = "none";
-        document.getElementById("soulsearch-inner").style.opacity = 0.5;
-        document.getElementById("soulsearch-inner").style.transition = "filter 1.5s ease" /* Transition effect for the filter property */
-        document.getElementById("soulsearch-inner").style.filter = "blur(1px)";
-        return
-    }
-    document.getElementById("soulsearch-inner").style.pointerEvents = "auto";
-    document.getElementById("soulsearch-inner").style.opacity = 1;
-    document.getElementById("soulsearch-inner").style.filter = "none";
 }
 
 function sendToSearch(query){
@@ -181,7 +162,7 @@ function display_result(result){
     if (type == 'text'){
         chap_text = 'Chapter'
     }
-    var h3_text = `A Segment from ${chap_text} ${result.chapter_id}`
+    var h3_text = `A Segment from ${chap_text} ${result.chapter}`
     if (result.chapter_title){
         var h3_text = `From <i>${result.chapter_title}</i>:`
     }
@@ -208,16 +189,11 @@ function display_result(result){
     if(guide_response){
         fullInner = `<p>${guide_response}</p>`
     }
-    fullInner = fullInner + `<div class="centered-content">`
-    var title_label = `<h2>${result.title}</h2>`
-    
+    var title_label = `<div class="centered-content"><h2>${result.title}</h2>`
     if (result.header_image_url){
-        title_label = `<a href="${cta_full}" target="_blank"><img src="${result.header_image_url}" width="100%"></a>`
+        title_label = `<div class="centered-content"><a href="${cta_full}" target="_blank"><img src="${result.header_image_url}" width="100%"></a>`
     }
-    let navigation = document.getElementById('soulsearch').getAttribute('navigation')
-    if (!navigation){
-        fullInner = fullInner + title_label
-    }
+    fullInner = fullInner + title_label
     var showh3 = ['video','audio', 'text'];
     if (showh3.includes(type)){
         fullInner = fullInner + `<h3 style="text-align:center" id="results-header">${h3_text}</h3>`
@@ -273,32 +249,11 @@ function performSearch() {
     //if search term, populate input with it
     document.getElementById('search-input').value = searchTerm
 
-    var filter = {}
-    if (document.getElementById('soulsearch').getAttribute('steward')){
-        filter.steward = document.getElementById('soulsearch').getAttribute('steward')
-    }
-    if (document.getElementById('soulsearch').getAttribute('teacher')){
-        filter.teacher = document.getElementById('soulsearch').getAttribute('teacher')
-    }
-
-    let carouselItems = document.getElementById('soulsearch').getAttribute('navigation')
-    carouselItems = decodeForHTMLAttribute(carouselItems, json=true)
+    var sources = decodeURIComponent(url.searchParams.get("s")).split(",");
+    var steward = document.getElementById('soulsearch').getAttribute('steward')
+    var teacher = document.getElementById('soulsearch').getAttribute('teacher')
     var guide = document.getElementById('soulsearch').getAttribute('guide')
-    let navigation = document.getElementById('soulsearch').getAttribute('navigation')
-    if(navigation){
-        var navItem = carouselItems[selectedItemIndex]
-        for (let key in navItem) {
-            if(key != 'img_url'){
-                filter[key] = navItem[key]
-                // console.log(key, yourobject[key]);
-            }
-        }
-    }
-    console.log('filter',filter)
 
-    const element = document.getElementById('search-form');
-    const height = element.offsetHeight;
-    document.getElementById("loader").style.top = `${height/2}px`
     document.getElementById("loader").style.display = "inline-block"
     document.getElementById("search-results").style.display = "none"
 
@@ -306,16 +261,16 @@ function performSearch() {
     var body = {
         query: searchTerm,
         // sources: sources,
-        filter:filter,
+        filter:{
+            steward: steward,
+            teacher: teacher,
+        },
         numberResults: 1,
         // display_sources: true,
         // url : url,
         gated:gated,
         guide: guide
     }
-
-    //freeze div
-    trigger_freeze(frozen=true)
     // Perform the API call
     fetch(`https://eon0klfitimzqd5.m.pipedream.net`,
     {
@@ -325,7 +280,6 @@ function performSearch() {
         .then(response => response.json())
         .then(data => {
             handle_results(data);
-            trigger_freeze(frozen=false)
         })
         .catch(error => {
         console.error('Error fetching data:', error);
@@ -377,15 +331,9 @@ function applyColorTheme() {
         button.style.backgroundColor = theme.primaryColor;
         button.style.color = theme.secondaryColor; // Assuming you want to change the text color too
         // button.onmouseover = () => button.style.backgroundColor = theme.secondaryColor;
-        // button.onmouseout = () => button.style.backgroundColor = theme.primaryColor;
+        // button.onmouseout = () => button.style.backgroundColor = theme.secondaryColor;
     });
 
-    //loader color
-    const loader = document.getElementById('loader')
-    
-    loader.style.borderImage = "linear-gradient(to right, red, yellow) 1 stretch"
-    loader.style.border = `16px solid ${theme.secondaryColor}`
-    loader.style.borderTop = `16px solid ${theme.primaryColor}`
 }
 
 var selectedItemIndex = 0
@@ -399,6 +347,7 @@ function changeSelectedItemIndex(direction){
     else{
         selectedItemIndex = (selectedItemIndex + 1) % carouselItems.length
     }
+    console.log(carouselItems[selectedItemIndex])
 }
 
 function moveCarousel(direction) {
@@ -453,6 +402,7 @@ function moveCarousel(direction) {
     
 }
 
+
 function addCarouselItems(){
     const carouselInner = document.querySelector('.ss-carousel-inner');
 
@@ -467,7 +417,7 @@ function addCarouselItems(){
         const carouselItem = document.createElement('div');
         carouselItem.className = 'ss-carousel-item';
         const img = document.createElement('img');
-        img.src = item.img_url; // Assuming that 'url' is where the image source is stored
+        img.src = item.src; // Assuming that 'url' is where the image source is stored
         img.alt = item.title; // Provide a meaningful description in the alt text
         carouselItem.appendChild(img);
         carouselInner.appendChild(carouselItem);
